f52d6337479ef39cfa472df2a9bb7c2e82a50f91
Justin Russo <j.russo@outlook.com>
2018-11-20 21:46:03 -0500

    Optionally allow modid's recipes to be staged using the item (#5)
    * feat: optionally allow modid's recipes to be staged using the item
    
    Setting stageRecipeWithItem to true allows the developer to specify that rather than staging all recipes created by a mod, it stages all recipes an item of that mod has.
    
    * refactor: stageRecipesWithItems for better grammar/readability
    
    * chore: add documentation for the logic contained in the TypeMod build method

--- src/main/java/uk/artdude/zenstages/stager/type/TypeMod.java
+++ src/main/java/uk/artdude/zenstages/stager/type/TypeMod.java
@@ -1,18 +1,56 @@
 package uk.artdude.zenstages.stager.type;
 
 import com.blamejared.recipestages.handlers.Recipes;
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.mc1120.item.MCItemStack;
+import net.darkhax.bookshelf.util.ModUtils;
+import net.darkhax.itemstages.compat.crt.ActionAddItemRestriction;
 import net.darkhax.itemstages.compat.crt.ItemStagesCrT;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class TypeMod extends TypeBase<String> {
+    private boolean stageRecipesWithItems;
 
-    public TypeMod(String modId) {
+    public TypeMod(String modId, boolean stageRecipesWithItems) {
         super(modId);
+
+        this.stageRecipesWithItems = stageRecipesWithItems;
     }
 
+    /**
+     * Both true and false for stageRecipesWithItems will result in recipes AND items being staged for the mod ID.
+     * When `stageRecipesWithItems == true`, to keep all the logic within a single loop, Item Staging is done
+     * individually rather than using `ItemStagesCrT.stageModItems(stageName, getValue())`
+     *
+     * When `Recipes.setRecipeStageByMod()` is called (when stageRecipesWithItems == false),
+     * RecipeStages searches the recipes registry for all recipes under the mod specified.
+     *
+     * Alternatively, with `stageRecipesWithItems == true`, the output item is passed to `Recipes.setRecipeStage`
+     * which results in RecipeStages searching for all recipes that output that item.
+     * This causes OTHER MOD's recipes for that item to ALSO get staged.
+     * This is ideal when modifying an item's recipes using CraftTweaker for example
+     * This behaviour is exactly the same as when calling ZenStages' addIngredient method.
+     */
     @Override
     public void build(String stageName) {
-        ItemStagesCrT.stageModItems(stageName, getValue());
-        Recipes.setRecipeStageByMod(stageName, getValue());
+        if (!stageRecipesWithItems) {
+            ItemStagesCrT.stageModItems(stageName, getValue());
+            Recipes.setRecipeStageByMod(stageName, getValue());
+            return;
+        }
+
+        for (final Item item : ModUtils.getSortedEntries(ForgeRegistries.ITEMS).get(getValue())) {
+            if (item != null && item != Items.AIR) {
+                // Stage Item
+                CraftTweakerAPI.apply(new ActionAddItemRestriction(stageName, item));
+
+                // Stage Item's recipes
+                Recipes.setRecipeStage(stageName, MCItemStack.createNonCopy(new ItemStack(item)));
+            }
+        }
     }
 
     @Override
